ca27b0b3736f62d033c9292e5be194a346459ed5
Martialcart <janolav91@hotmail.com>
2024-10-24 11:29:49 +0200

    etter pause

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-console.log('Hello!');
+console.log('Hello world!'); // '' = ""
+
+//Variables. Use english names because of æøå problems
+//camelcase recomended
+
+let age = 28; //kan endre seg
+const firstName = "Julie"; //konstant
+
+//boolean
+const isPresent = true;
+console.log(typeof(isPresent));
+
+//string chaining/ concatenation(java)
+console.log(firstName + " " + age); // +            
+console.log("hei, jeg heter", firstName); //komma   chaining
+console.log(`min alder er ${age}`);// `` med ${}    template literal
+
+//Show types. farge gjenspeiler type
+console.log(typeof(age));
+console.log(typeof(firstName));
+
+const surName = "Hauge";
+const fullname = firstName + " " + surName;  //parent
+//string methods seinere
+console.log(fullname);
